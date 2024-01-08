a76ab8d570ff6129d9d84cfcd12c208b05329bc5
ErkinArikan <erkinarikan@gmail.com>
2024-01-08 18:25:51 +0300

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Erkin Arikan on 8.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
